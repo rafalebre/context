0461ee7f3527c804e72e8008a3d36b48ff0ecfb2
Rafael Lebre <rafaellebre100@gmail.com>
2023-04-11 16:47:28 +0200

    Desafio 8 concluído

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BroswerRouter, Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import Products from './pages/Products';
 import About from './pages/About';
