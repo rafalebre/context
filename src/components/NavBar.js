c23d39eae5d72b8e07f3bc123c5a514dd1b7d0d5
Rafael Lebre <rafaellebre100@gmail.com>
2023-04-11 16:31:22 +0200

    Desafio 8

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import { NavLink } from "react-router-dom";
+
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/products">Products</NavLink>
+        <NavLink to="/about">About</NavLink>
+    </nav>
+  )
+}
+
+export default NavBar;
