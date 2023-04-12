9824f1d00296af87a1e93a90dab90442d51ce116
Rafael Lebre <rafaellebre100@gmail.com>
2023-04-12 17:49:09 +0200

    Aula 112 - Refatorando contexto para hook

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
-import { useContext } from "react";
-
-import {CounterContext} from "../context/CounterContext";
+// import { useContext } from "react";
+// import {CounterContext} from "../context/CounterContext";
 import ChangeCounter from "../components/ChangeCounter";
 
-const Home = () => {
-  const { counter } = useContext(CounterContext);
+// 4 - refatorando com hook
+import { useCounterContext } from "../hooks/useCounterContext";
 
+const Home = () => {
+  // const { counter } = useContext(CounterContext);
+  const {counter} = useCounterContext();
+  
   return (
     <div>
       <h1>Home</h1>
